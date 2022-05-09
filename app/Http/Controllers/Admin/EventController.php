5638b2a3d12231c6089fa760a4a65af095598570
Victor Nogueira <victornogu80@gmail.com>
2022-05-09 11:50:30 -0300

    Correção do envio de arquivos ao atualizar

--- app/Http/Controllers/Admin/EventController.php
+++ app/Http/Controllers/Admin/EventController.php
@@ -67,6 +67,8 @@ class EventController extends Controller
             $file = $request->file('image')->store('events');
         }
 
+        dd($file);
+
         $event->update([
             'title' => $request->get('title'),
             'description' => $request->get('description'),
